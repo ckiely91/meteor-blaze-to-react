aa74c54f46baec043d760de96571de532d1b99c9
Christian Kiely <ckiely91@gmail.com>
2016-04-16 12:29:50 +1000

    Changes for my own usage

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'gwendall:blaze-to-react',
+  name: 'gwendall:blaze-to-react-ck',
   summary: 'Turn any Blaze template into a React component',
   git: 'https://github.com/gwendall/meteor-blaze-to-react.git',
   version: '0.1.2'
@@ -8,8 +8,9 @@ Package.describe({
 Package.onUse(function(api, where) {
   api.versionsFrom('1.2');
   api.use([
-    'react@0.14.1_1',
-    'templating@1.1.5', 
+    'jsx@0.2.4',
+    'react-meteor-data@0.2.5',
+    'templating@1.1.5',
     'underscore@1.0.4'
   ]);
   api.addFiles('lib.jsx');
